fe15468e001166b9f8f29df06a434b0e52eaa542
HyeongJunMin <alsgudwns007@naver.com>
2021-04-18 16:41:57 +0900

    멀티모듈 프로젝트 초기설정 core, web, api

--- core/src/test/java/com/intro/core/CoreApplicationTests.java
+++ core/src/test/java/com/intro/core/CoreApplicationTests.java
@@ -1,13 +1,16 @@
 package com.intro.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class CoreApplicationTests {
 
   @Test
   void contextLoads() {
+    log.info("core done");
   }
 
 }
